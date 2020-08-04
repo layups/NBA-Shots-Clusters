1ce44852858508eea94a4bdefbd400fb3e4af6bd
layups <68927170+layups@users.noreply.github.com>
2020-08-04 17:38:26 -0300

    R Script for scraping the data from NBA.com

--- shot_chart_scrap.R
+++ shot_chart_scrap.R
@@ -2,9 +2,7 @@ library(tidyverse)
 library(NBAr)
 
 players <- get_players(2019) %>% pull(player_id)
-
 shots <- lapply(players,get_shotchart,season=2019)
-
 data <- data.frame()
 for(i in 1:540){
   data <- bind_rows(data,shots[[i]])
